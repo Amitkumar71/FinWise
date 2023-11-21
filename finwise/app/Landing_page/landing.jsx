59fd73106ad232b2c5e41b33abb3ff318a235466
Anas saifi <anassaifi1108@gmail.com>
2023-11-21 23:40:08 +0530

    Changes on landing page

--- finwise/app/Landing_page/landing.jsx
+++ finwise/app/Landing_page/landing.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import Image from 'next/image'
 import styles from './Landing.module.css';
-// import image from "/photu.png";
-
+import landingImg from './landingimg.svg'
 const Landing = () => {
   return (
     <div className={styles.landing}>
-        <div className={styles.content}>
-          <img src={"/photu-2.png"} alt="logo" />
-          <div className={styles.data}>
+      <div className={styles.content}>
+        <Image src={landingImg} alt="logo" height={500} width={500}/>
+        <div className={styles.data}>
           <h1>Auto-Pilot Your Finances with Finwise</h1>
           <p>Finwise securely connects to your Bank accounts and gives a clear picture of your finances. So you can make better decisions and lead a healthier financial life.</p>
-          </div>
-          <div className={styles.QrCode}>
-        <img src={"/qrcode.svg"} alt="logo" />
         </div>
+        <div className={styles.QrCode}>
+          <img src={"/qrcode.svg"} alt="logo" />
         </div>
+      </div>
     </div>
   );
 }
--- finwise/app/Navbar/navbar.jsx
+++ finwise/app/Navbar/navbar.jsx
@@ -1,30 +1,33 @@
 // Navbar.js
 import Link from 'next/link';
+import Image from 'next/image';
+import TeamIcon from './team-fill.svg';
+import blogIcon from './article-line.svg';
 import './navbar.css';
+import logo from './logo.svg'
 
 export default function Navbar() {
   return (
-    <>
     <nav className="navbar">
-    <div className="logo">
-        <img src="/logo.png" />
-        <Link href="/">Finwise</Link>
-        </div> 
-        <div className='nav-right'>
-      <Link href="/">
-        Home
-      </Link>
-      <Link href="/component1">
-        Team 
-      </Link>
-      <Link href="/component2">
-        Blog
-      </Link>
-      <Link href="/component3">
-        Privacy
-      </Link>
+      <div className="logo">
+        <Image src={logo} alt="Finwise Logo" width={40} height={40} />
+      </div>
+      <div className='nav-right'>
+
+        <Link href="/component1">
+          <div className="team-link">
+            <Image src={TeamIcon} alt="Team Icon" width={20} height={20} />
+            <span>Team</span>
+          </div>
+        </Link>
+        <Link href="/component2">
+        <div className="article-link">
+        <Image src={blogIcon} alt="Blog Icon" width={20} height={20} />
+        <span>Blog</span>
+        </div>
+        </Link>
+        <Link href="/component3">Privacy</Link>
       </div>
     </nav>
-    </>
   );
 }
